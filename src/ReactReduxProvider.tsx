ab079280694e4d95d5a3ec5ba3d59b2e5521ce8d
Cat4848 <catalinrol@gmail.com>
2024-11-02 18:31:49 +0000

    feat: added ReactReduxProvider

--- src/ReactReduxProvider.tsx
+++ src/ReactReduxProvider.tsx
@@ -1,2 +1,13 @@
+"use client";
+
 import { Provider } from "react-redux";
 import { store } from "./app/redux/store/createStore";
+import { ReactNode } from "react";
+
+export default function ReactReduxProvider({
+  children
+}: {
+  children: ReactNode;
+}) {
+  return <Provider store={store}>{children}</Provider>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import Link from "next/link";
 import ReactQueryProvider from "@/ReactQueryProvider";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Provider } from "react-redux";
+import ReactReduxProvider from "@/ReactReduxProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,17 +18,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ReactQueryProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <nav>
-            <Link href="/">{"Home"}</Link>
-            <Link href="posts">{"All Posts"}</Link>
-          </nav>
-          {children}
-          <ReactQueryDevtools />
-        </body>
-      </html>
-    </ReactQueryProvider>
+    <ReactReduxProvider>
+      <ReactQueryProvider>
+        <html lang="en">
+          <body className={inter.className}>
+            <nav>
+              <Link href="/">{"Home"}</Link>
+              <Link href="posts">{"All Posts"}</Link>
+            </nav>
+            {children}
+            <ReactQueryDevtools />
+          </body>
+        </html>
+      </ReactQueryProvider>
+    </ReactReduxProvider>
   );
 }
