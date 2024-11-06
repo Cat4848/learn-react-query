ccb7139024194ea384c049d8f084ecd44424db2c
Cat4848 <catalinrol@gmail.com>
2024-11-06 05:06:14 +0000

    closures testing

--- src/app/redux/reducer.ts
+++ src/app/redux/reducer.ts
@@ -4,10 +4,7 @@ interface Store {
   value: number;
 }
 
-export default function reducer(
-  state: Reducer<Store>,
-  action: { type: string }
-) {
+export default function reducer(state: Store, action: { type: string }) {
   switch (action.type) {
     case "increment":
       return {
@@ -19,3 +16,16 @@ export default function reducer(
       return state;
   }
 }
+
+const nextFunct = (input: string) => {
+  console.log("log from next function -> ", input);
+};
+
+const print1 = (greeting: string) => (next: (input: string) => void) => {
+  console.log("print 1");
+  next(greeting);
+};
+
+const innerFunct = print1("hello 1");
+const res = innerFunct(nextFunct);
+console.log("res", res);
