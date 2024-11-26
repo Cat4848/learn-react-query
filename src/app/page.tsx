ad78692264a47b2465f503b41ba05da0f31a6a7a
Cat4848 <catalinrol@gmail.com>
2024-11-26 04:02:37 +0000

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import { store } from "./redux/store/createStore";
 import { useSelector, UseSelector } from "react-redux";
 
 export default function Home() {
-  const state = useSelector<{value: number}>((state) => state);
+  const state = useSelector<{ value: number }>((state) => state);
   return (
     <div>
       <h1>Home Page</h1>
-      <div>store value {state}</div>
+      {/* <div>store value {state}</div> */}
       <button onClick={() => store.dispatch({ type: "increment" })}>
         Increment
       </button>
