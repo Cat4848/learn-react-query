1b1f09d6ac6d484c72b03e2b96bb121fcc42a797
Cat4848 <catalinrol@gmail.com>
2025-10-11 21:15:10 +0100

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      {/* <div>store value {state}</div> */}
+    /  <div>store value {state}</div>
       <button onClick={() => store.dispatch({ type: "increment" })}>
         Increment
       </button>
