3fdc79a67a479938114f97322ca1c4e1b57e492a
Cat4848 <catalinrol@gmail.com>
2026-09-14 05:19:50 +0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { store } from "./redux/store/createStore";
+// import { store } from "./redux/store/createStore";
 import { useSelector, useSelector } from "react-redux";
 
 export default function Home() {
